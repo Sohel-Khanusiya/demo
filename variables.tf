b0212940f440907752879495a797df088697a6e1
Sohel-Khanusiya <127925087+Sohel-Khanusiya@users.noreply.github.com>
2024-02-13 16:28:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "instance_name1" {
+  type  = string
+  default  = "kali-rdp"
+}
+
+variable "instance_name3" {
+  type  = string  
+  default  = "Windows-10-Pro"
+}
+
+variable "keypair_name" {
+  type = string
+  default = "CloudLab-Xavier-Engineering123"
+}
